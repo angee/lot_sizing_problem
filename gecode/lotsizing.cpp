19755eb337e75d9873b36dbd66a11a453b59b43b
Andrea Rendl <andrea.rendl@gmail.com>
2019-09-10 19:02:02 +0200

    some extensions of instance reader

--- gecode/lotsizing.cpp
+++ gecode/lotsizing.cpp
@@ -2,6 +2,8 @@
 #include <gecode/int.hh>
 #include <gecode/minimodel.hh>
 #include <fstream>
+#include <sstream>
+#include <string>
 
 using namespace Gecode;
 
@@ -34,8 +36,84 @@ using namespace Gecode;
  * Author: Andrea Rendl, July 2019
  *
  */
+
+class LotSizingInstance {
+
+ private:
+  int num_periods;
+  int num_types;
+  int num_orders;
+
+  /** due period for each order*/
+  std::vector<int> due_period_per_order;
+  /** the number of orders per item type */
+  std::vector<int> num_orders_per_type;
+
+ public:
+  LotSizingInstance(std::string input_filename) {
+    readInputFile(input_filename);
+  }
+
+ private:
+  void readInputFile(const std::string input_filename) {
+    std::ifstream infile(input_filename);
+    if (!infile.good()) {
+      std::stringstream error_msg;
+      error_msg << "Cannot open file: " << input_filename;
+      throw std::runtime_error(error_msg.str());
+    }
+    infile >> num_periods;
+    infile >> num_types;
+
+    readOrders(infile);
+  }
+
+  void readOrders(std::ifstream &infile) {
+    int order_cnt = 0;
+    int type_cnt = 0;
+
+    std::string line;
+    while(type_cnt < num_types && std::getline(infile, line)) {
+      std::getline(infile, line);
+      std::stringstream iss(line);
+      int order_cnt_per_type = 0;
+      // read the line for the item type
+      int period_cnt = 0;
+      int hasOrder;
+      while(period_cnt < num_periods && iss >> hasOrder) {
+        //std::cout << "hasOrder of " << period_cnt << ": " << hasOrder << std::endl;
+        if (hasOrder == 1) {
+          due_period_per_order.push_back(period_cnt + 1);
+          order_cnt++;
+          order_cnt_per_type++;
+        }
+        period_cnt++;
+      }
+      num_orders_per_type.push_back(order_cnt_per_type);
+      type_cnt++;
+    }
+    num_orders = order_cnt;
+  }
+
+ public:
+  void print() const {
+    std::cout << "#Periods = " << num_periods << std::endl;
+    std::cout << "#ItemTypes = " << num_types << std::endl;
+    std::cout << "#Orders = " << num_orders << std::endl;
+    std::cout << "\ndue_periods = ";
+    for (auto i = due_period_per_order.begin(); i != due_period_per_order.end(); ++i)
+      std::cout << *i << " ";
+    std::cout << std::endl;
+    std::cout << "\nnum_order_per_item = ";
+    for (auto i = num_orders_per_type.begin(); i != num_orders_per_type.end(); ++i)
+      std::cout << *i << " ";
+    std::cout << std::endl;
+  }
+};
+
 class LotSizing : public IntMinimizeScript {
  protected:
+  LotSizingInstance instance;
   IntVarArray production_by_order;
   IntVarArray production_period;
   IntVarArray inventory_periods;
@@ -43,8 +121,9 @@ class LotSizing : public IntMinimizeScript {
   IntVarArray production_order; // FIXME: can it be an IntVarArgs array?
 
  public:
-  LotSizing(const InstanceOptions &opt) : IntMinimizeScript(opt) {
-    readInputFile(opt.instance());
+  LotSizing(const InstanceOptions &opt) : IntMinimizeScript(opt), instance(opt.instance()) {
+    instance.print();
+    // TODO
   }
 
   virtual Space *copy(void) {
@@ -55,21 +134,5 @@ class LotSizing : public IntMinimizeScript {
     // TODO
   }
 
- private:
-  void readInputFile(const std::string input_filename) {
-    std::ifstream infile(input_filename);
-    if(!infile.good()) {
-      std::stringstream error_msg;
-      error_msg << "Cannot open file: " << input_filename;
-      throw std::runtime_error(error_msg.str());
-    }
-
-    int num_timeslots = 0;
-    int num_types = 0;
-    infile >> num_timeslots;
-    infile >> num_types;
-
-    std::cout << "#timeslots = " << num_timeslots << std::endl;
-  }
-
 };
+
