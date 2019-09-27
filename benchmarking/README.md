acbe079eb8a1382a186d77ac7c7322931ba16dd3
Andrea Rendl <andrea.rendl@gmail.com>
2019-09-27 13:08:05 +0200

    adds Bash script for running experiments

---
+++ benchmarking/README.md
@@ -0,0 +1,11 @@
+### Benchmarking scripts 
+
+This folder contains scripts for running benchmarks on the examples and the solving algorithms,
+as well as for generating graphs.
+
+
+#### Running the scripts
+
+The scripts are written in Python using Python 3.5.
+
+ 
---
+++ benchmarking/run_experiments.sh
@@ -0,0 +1,71 @@
+#!/usr/bin/env bash
+# ==================================================================================================================== #
+#
+# NOTE: Run the tests in _this_ directory, because you will need the csv files in the regressionTestFiles/ directory
+#       to compare the results of your test runs.
+#
+# ==================================================================================================================== #
+
+CUR_DIR=`pwd`
+
+# put here the path to your lotsizing build directory
+PATH_TO_EXECUTABLE=${CUR_DIR}/../gecode/build/
+
+# the executable of the solving approach
+EXECUTABLE=${PATH_TO_EXECUTABLE}lotsizing
+
+# ==================================================================================================================== #
+
+# Specify what features we want to test in this benchmark run
+
+declare -a search_options=("exact" "lns")
+declare -a branching_options=("base" "greedy" "greedy-dynamic") 
+#declare -a model_options=("base" "redundant")         # this will probably be ignored
+declare -a seed_options=(1 7 11) 
+declare -a lns_destruction_rates=(0.5 0.7 0.9) 
+
+NUM_SEARCH=${#search_options[@]}            # number of search options
+NUM_BRANCHING=${#branching_options[@]}      # number of branching options
+NUM_SEEDS=${#seeds_options[@]}              # number of seed options
+NUM_LNS_RATE=${#lns_destruction_rates[@]}   # number of LNS destruction rates
+
+TIME_OUT=1000  # the timeout in milliseconds
+
+# ==================================================================================================================== #
+
+# edit here the pattern matching expression to narrow down the examples to run
+for problemfile in ../examples/txt/pigment20a.psp; do
+    
+    for ((s=0;s<=$NUM_SEARCH-1;s++)); do
+	search=${search_options[$s]}
+	
+	for ((b=0;b<=$NUM_BRANCHING-1;b++)); do	    
+	    branching=${branching_options[$b]}
+	    
+	    # Solving via LNS
+	    if [[ "${search}" == "lns" ]]
+	    then
+		echo "----- LNS -----"
+		for ((r=0;r<=$NUM_LNS_RATE-1;r++)); do
+		    lns_rate=${lns_destruction_rates[$r]}
+		    solution_file=${problemfile}_${search}_${branching}_${lns_rate}.sol
+		    #stats_file=${problemfile}_${search}_${branching}_${lns_rate}.stats
+		    
+		    echo "${EXECUTABLE} --time $TIME_OUT --search lns --relax ${lns_destruction_rates[$r]} --branching ${branching} ${problemfile} > ${solution_file}"
+		    ${EXECUTABLE} --time $TIME_OUT --search lns --relax ${lns_destruction_rates[$r]} --branching ${branching} ${problemfile} > ${solution_file}
+		done
+		
+	    # Solving exactly
+	    else
+		echo "----- EXACT -----"
+		solution_file=${problemfile}_${search}_${branching}.sol
+		echo "solution file = ${solution_file}"
+		stats_file=${problemfile}_${search}_${branching}.stats
+		
+		echo "${EXECUTABLE} --time $TIME_OUT --search ${search} --branching ${branching} --file-sol ${solution_file} -file-stat ${stats_file} ${problemfile}"
+		${EXECUTABLE} --time $TIME_OUT --search ${search} --branching ${branching} -file-sol ${solution_file} -file-stat ${stats_file} ${problemfile}
+	    fi
+	done
+    done
+    
+done
